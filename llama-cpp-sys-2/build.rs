8317034dd66323c14707b373f5190c5c506b9d03
Kartik Nair <kartikajitnair@gmail.com>
2024-08-19 18:58:39 +0400

    try changing nvidia arch for smaller build

--- llama-cpp-sys-2/build.rs
+++ llama-cpp-sys-2/build.rs
@@ -91,11 +91,9 @@ fn compile_bindings(
     llama_header_path: &Path,
 ) -> Result<(), Box<dyn std::error::Error + 'static>> {
     println!("Generating bindings..");
-    
-    let includes = [
-        llama_header_path.join("ggml").join("include"),
-    ];
-    
+
+    let includes = [llama_header_path.join("ggml").join("include")];
+
     let bindings = bindgen::Builder::default()
         .clang_args(includes.map(|path| format!("-I{}", path.to_string_lossy())))
         .header(
@@ -417,7 +415,7 @@ fn compile_cuda(cx: &mut Build, cxx: &mut Build, featless_cxx: Build) -> &'stati
     let mut nvcc = featless_cxx;
     nvcc.cuda(true)
         .flag("--forward-unknown-to-host-compiler")
-        .flag("-arch=all")
+        .flag("-arch=all-major")
         .define("K_QUANTS_PER_ITERATION", Some("2"))
         .define("GGML_CUDA_PEER_MAX_BATCH_SIZE", Some("128"));
 
@@ -425,9 +423,7 @@ fn compile_cuda(cx: &mut Build, cxx: &mut Build, featless_cxx: Build) -> &'stati
     //     nvcc.flag("-Wno-pedantic");
     // }
 
-    for lib in [
-        "cuda", "cublas", "cudart", "cublasLt"
-    ] {
+    for lib in ["cuda", "cublas", "cudart", "cublasLt"] {
         println!("cargo:rustc-link-lib={}", lib);
     }
     if !nvcc.get_compiler().is_like_msvc() {
@@ -467,7 +463,16 @@ fn compile_cuda(cx: &mut Build, cxx: &mut Build, featless_cxx: Build) -> &'stati
     let template_instances = read_dir(cuda_path.join("template-instances"))
         .unwrap()
         .map(|f| f.unwrap())
-        .filter(|entry| entry.file_name().to_string_lossy().ends_with(".cu"))
+        .filter(|entry| {
+            let name = entry.file_name().to_string_lossy().to_string();
+            if name.starts_with("fattn") {
+                name.ends_with("q4_0-q4_0.cu")
+                    || name.ends_with("q8_0-q8_0.cu")
+                    || name.ends_with("f16-f16.cu")
+            } else {
+                name.ends_with(".cu")
+            }
+        })
         .map(|entry| entry.path());
 
     nvcc.include(cuda_path.as_path())
@@ -623,31 +628,44 @@ fn gen_vulkan_shaders(out_path: impl AsRef<Path>) -> (impl AsRef<Path>, impl AsR
         .cpp(true)
         .get_compiler();
 
-    assert!(!cxx.is_like_msvc(), "Compiling Vulkan GGML with MSVC is not supported at this time.");
+    assert!(
+        !cxx.is_like_msvc(),
+        "Compiling Vulkan GGML with MSVC is not supported at this time."
+    );
 
     let vulkan_shaders_gen_bin = out_path.as_ref().join("vulkan-shaders-gen");
 
     cxx.to_command()
         .args([
-            vulkan_shaders_src.join("vulkan-shaders-gen.cpp").as_os_str(),
-            "-o".as_ref(), vulkan_shaders_gen_bin.as_os_str()
+            vulkan_shaders_src
+                .join("vulkan-shaders-gen.cpp")
+                .as_os_str(),
+            "-o".as_ref(),
+            vulkan_shaders_gen_bin.as_os_str(),
         ])
-        .output().expect("Could not compile Vulkan shader generator");
+        .output()
+        .expect("Could not compile Vulkan shader generator");
 
     let header = out_path.as_ref().join("ggml-vulkan-shaders.hpp");
     let source = out_path.as_ref().join("ggml-vulkan-shaders.cpp");
 
     Command::new(vulkan_shaders_gen_bin)
         .args([
-            "--glslc".as_ref(), "glslc".as_ref(),
-            "--input-dir".as_ref(), vulkan_shaders_src.as_os_str(),
-            "--output-dir".as_ref(), out_path.as_ref().join("vulkan-shaders.spv").as_os_str(),
-            "--target-hpp".as_ref(), header.as_os_str(),
-            "--target-cpp".as_ref(), source.as_os_str(),
-            "--no-clean".as_ref()
+            "--glslc".as_ref(),
+            "glslc".as_ref(),
+            "--input-dir".as_ref(),
+            vulkan_shaders_src.as_os_str(),
+            "--output-dir".as_ref(),
+            out_path.as_ref().join("vulkan-shaders.spv").as_os_str(),
+            "--target-hpp".as_ref(),
+            header.as_os_str(),
+            "--target-cpp".as_ref(),
+            source.as_os_str(),
+            "--no-clean".as_ref(),
         ])
-        .output().expect("Could not run Vulkan shader generator");
-    
+        .output()
+        .expect("Could not run Vulkan shader generator");
+
     (out_path, source)
 }
 
